0a7a8b6c8e0bfbdcd1372e2e84df19d58da42c3c
Titus Yau <tituschunyau@gmail.com>
2022-09-06 22:38:33 +0800

    Made some changes to index.html

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+# Basic function that returns a username
 
-class User(models.Model):
-    username = models.CharField(max_length=20)
+
+class Username(models.Model):
+    username = models.CharField(max_length=30)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .models import *
 # Create your views here.
 
 
 def index(request):
-
-    return render(request, 'index.html')
+    usernames = Username.objects.all()
+    context = {'usernames': usernames}
+    return render(request, 'dashboard/index.html', context)
