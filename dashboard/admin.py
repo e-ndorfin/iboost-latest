8672aa5180941535d1bce831e1ea261e601d4983
TitusYau <tituschunyau@gmail.com>
2022-09-21 16:18:40 +0800

    Created grade model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+admin.site.register(Grade)
 admin.site.register(Subject)
 admin.site.register(User)
 
--- dashboard/models.py
+++ dashboard/models.py
@@ -16,19 +16,24 @@ from django.db import models
 #         return self.username
 
 # Actual Code:
+class Grade(models.Model):
+    # Database fields
+    criterionA = models.IntegerField(default=0, null=True)
+    criterionB = models.IntegerField(default=0, null=True)
+    criterionC = models.IntegerField(default=0, null=True)
+    criterionD = models.IntegerField(default=0, null=True)
+    
 class Subject(models.Model):
     # Database fields
     subjectname = models.CharField(max_length=30, null=True)
-    score = models.IntegerField(default=0, null=True)
+    score = models.ManyToManyField(Grade)
 
     def __str__(self):
         return self.subjectname
 
-
 class User(models.Model):
     # Database Fields
     username = models.CharField(max_length=30, null=True)
-    grade = models.IntegerField(default=0, null=True)
     subjects = models.ManyToManyField(Subject)
 
     def __str__(self):
