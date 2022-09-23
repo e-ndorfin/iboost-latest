a85f76c0fd8e05d26f5a973e485c18df3a613b02
Titus Yau <tituschunyau@gmail.com>
2022-09-23 22:19:28 +0800

    Revamped model structure

--- dashboard/models.py
+++ dashboard/models.py
@@ -16,25 +16,32 @@ from django.db import models
 #         return self.username
 
 # Actual Code:
-class Grade(models.Model):
-    # Database fields
-    criterionA = models.IntegerField(default=0, null=True)
-    criterionB = models.IntegerField(default=0, null=True)
-    criterionC = models.IntegerField(default=0, null=True)
-    criterionD = models.IntegerField(default=0, null=True)
-    
+class User(models.Model):
+    # Database Fields
+    username = models.CharField(max_length=30, null=True)
+    # subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.username
+
+
 class Subject(models.Model):
     # Database fields
+    # User has a "One to Many" relationship with Subject, as one User can have multiple subjects,while the opposite is not the case
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     subjectname = models.CharField(max_length=30, null=True)
-    score = models.ManyToManyField(Grade)
 
     def __str__(self):
         return self.subjectname
 
-class User(models.Model):
-    # Database Fields
-    username = models.CharField(max_length=30, null=True)
-    subjects = models.ManyToManyField(Subject)
+
+class Grade(models.Model):  # Subject has a "One to Many" relationship with Grade, as one User can have multiple subjects,while the opposite is not the case
+    # Database fields
+    subject = models.ForeignKey(Subject, null=True, on_delete=models.SET_NULL)
+    criterionA = models.IntegerField(default=0, null=True)
+    criterionB = models.IntegerField(default=0, null=True)
+    criterionC = models.IntegerField(default=0, null=True)
+    criterionD = models.IntegerField(default=0, null=True)
 
     def __str__(self):
-        return self.username
+        return "A:%i B:%i C:%i D:%i" % (self.criterionA, self.criterionB, self.criterionC, self.criterionD)
