e4b9a098128f948ba5b140a50d5c98d9f2a64ed5
Titus Yau <tituschunyau@gmail.com>
2023-01-16 16:05:36 +0800

    Rudimentary grades page

---
+++ dashboard/static/frontend/addgrades.js
@@ -0,0 +1,10 @@
+function addgrades(id, id2) {
+    let mainbody = document.getElementById(id);
+    let addgrades = document.getElementById(id2);
+    mainbody.style.display = "none";
+    addgrades.style.display = "block";
+}
+
+function test() {
+    console.log("test");
+}
