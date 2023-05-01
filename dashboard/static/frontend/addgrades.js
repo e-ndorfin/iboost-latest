579f392cbef3a2b0eb7f09e1669acb49144adbad
e-ndorfin <agarioloki@gmail.com>
2023-05-02 01:00:07 +0800

    added x to ADD GRADES page

--- dashboard/static/frontend/addgrades.js
+++ dashboard/static/frontend/addgrades.js
@@ -1,10 +1,17 @@
 function addgrades(id, id2) {
-    let mainbody = document.getElementById(id);
-    let addgrades = document.getElementById(id2);
-    mainbody.style.display = "none";
-    addgrades.style.display = "block";
+	let mainbody = document.getElementById(id);
+	let addgrades = document.getElementById(id2);
+	mainbody.style.display = "none";
+	addgrades.style.display = "block";
+}
+
+function removegrades(id, id2) {
+	let mainbody = document.getElementById(id);
+	let addgrades = document.getElementById(id2);
+	mainbody.style.display = "block";
+	addgrades.style.display = "none";
 }
 
 function test() {
-    console.log("test");
+	console.log("test");
 }
