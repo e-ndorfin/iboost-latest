bb7bc1c9698fe7f76c239b8c486cc441279b817f
Titus Yau <tituschunyau@gmail.com>
2023-06-23 13:43:32 +0800

    Added video generation for weakest criterion

--- dashboard/static/frontend/subject.js
+++ dashboard/static/frontend/subject.js
@@ -1,17 +1,46 @@
 //Script for determining which videos to show improvement area/ improvement text
 var subject = document.getElementById('subjectname').textContent.toLowerCase().replaceAll(" ","");
-console.log(subject)
 
-const math = ['<iframe width="560" height="315" src="https://www.youtube.com/embed/M9Rrf9-yWxw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', '<iframe width="560" height="315" src="https://www.youtube.com/embed/CAtw6yDRPe4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', '<iframe width="560" height="315" src="https://www.youtube.com/embed/AEtk2xNQlno" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', '<iframe width="560" height="315" src="https://www.youtube.com/embed/wUlZjCYIVsE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'];
-const computerscience = ['<iframe width="560" height="315" src="https://www.youtube.com/embed/b4pCjnAE7zk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>, <iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/f7JwN0gr_ww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/f7JwN0gr_ww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen ></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/f7JwN0gr_ww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen ></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/C6jVcqBacO8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'];
-const individualsandsocieties = [];
-const chinese = [];
-const english = [];
-const science = [];
-const music = [];
-const visualarts = [];
-const designtechnology = [];
+const math = ['<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/M9Rrf9-yWxw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/CAtw6yDRPe4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/AEtk2xNQlno" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/wUlZjCYIVsE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>','math'];
+const computerscience = ['<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/b4pCjnAE7zk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>, <iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/f7JwN0gr_ww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/f7JwN0gr_ww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen ></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/f7JwN0gr_ww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen ></iframe>', '<iframe style="width: 100%; height: 25vh;" src="https://www.youtube.com/embed/C6jVcqBacO8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>','computerscience'];
+const individualsandsocieties = ['individualsandsocieties'];
+const chinese = ['chinese'];
+const english = ['english'];
+const science = ['science'];
+const music = ['music'];
+const visualarts = ['visualarts'];
+const designtechnology = ['designtechnology'];
 
+const mathfb = [];
+const computersciencefb = [];
+const individualsandsocietiesfb = [];
+const chinesefb = [];
+const englishfb = [];
+const sciencefb = [];
+const musicfb = [];
+const visualartsfb = [];
+const designtechnologyfb = [];
 
+const videoarray = [math, computerscience, individualsandsocieties, chinese, english, science, music, visualarts, designtechnology];
+const feedbackarray = [];
+//Find weakest criterion:
+var criterionavgtemp = document.getElementById('criterionavg').textContent.replaceAll(" ", "").replaceAll("[", "").replaceAll("]", "").split(',');
+var criterionavg = [];
+for(grade in criterionavgtemp) {
+    criterionavg.push(parseFloat(criterionavgtemp[grade]));
+}
+var weakcrit = Math.min.apply(Math,criterionavg);
+var weakcritindex = 0;
 
+for(grade in criterionavg){
+    if(criterionavg[grade] == weakcrit){
+        weakcritindex = grade;
+        break;
+    }
+}
+
+for(item in videoarray){
+    if(subject == videoarray[item].slice(-1))
+        document.getElementById("video").innerHTML += videoarray[item][weakcritindex];
+}
 
