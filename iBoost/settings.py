e5dd264c94b1ff80f1f63b9ae0c72c16d15aefbf
W11nter <99022029+W11nter@users.noreply.github.com>
2023-08-07 20:58:23 +0800

    added staticfiles_dirs

--- iBoost/settings.py
+++ iBoost/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,6 +137,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATICFILES_DIRS = [
+    "/dashboard/static/",
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 STATIC_URL = 'static/'
 SITE_ID = 3
@@ -164,3 +170,4 @@ LOGGING = {
         },
     },
 }
+
